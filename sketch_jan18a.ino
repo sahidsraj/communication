d5ac85bab0c34c71e276102efc2377baed65a308
sahidsraj <sahidsraj@users.noreply.github.com>
2018-01-19 00:33:52 +0530

    random
    communication

---
+++ sketch_jan18a.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available() > 0)
+  {
+    char letter = Serial.read();
+    if(letter == '1')
+    {
+      digitalWrite(13,HIGH);
+      Serial.println("Arduino is on");
+      
+    }
+    else if(letter == '0')
+    {
+      digitalWrite(13,LOW);
+      Serial.println("Arduino is off");
+    }
+  }
+
+}
